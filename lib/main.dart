7d3b880cceb169a840d65dc4418d4fb2ee38dcee
dragan1810 <d.novakovic1810@yahoo.com>
2022-09-17 16:55:04 +0200

    qqq

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:ff/blocs/rootBloc.dart';
 import 'package:ff/pages/Login.dart';
 import 'package:firebase_core/firebase_core.dart';
-//import 'package:ff/pages/Missions.dart';
 import 'package:flutter/material.dart';
-
-// import 'pages/Inventory.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(MyApp());
 }
